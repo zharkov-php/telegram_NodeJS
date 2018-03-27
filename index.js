1e64db12ca9db7a324f24c78a10b2f5dcb1742e2
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-27 15:37:59 +0300

    Вывод ссылки в браузер

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const TelegramBot = require('node-telegram-bot-api')
+ const TelegramBot = require('node-telegram-bot-api')
+ const debug = require('./helpers')
+ const TOKEN = '595808254:AAHjcdRI0-wohBLK9_xUf7PV2cbWG_Kse_w'
 
-const debug = require('./helpers')
-const TOKEN = '595808254:AAHjcdRI0-wohBLK9_xUf7PV2cbWG_Kse_w'
-
-const bot = new TelegramBot(TOKEN, {
+ const bot = new TelegramBot(TOKEN, {
     polling: {
         interval: 300,//интервал загрукзки между клинтом и сервером
         autoStart: true,//если бот незапущен, он запоминает команды и потом после запуска ответит
@@ -13,16 +12,10 @@ const bot = new TelegramBot(TOKEN, {
     }
 })
 
-bot.on('message', msg => {
 
-    const markdown= `
-*Hello, ${msg.from.first_name}*
-__Твое имя ${msg.from.last_name}__
-Твой ID  ${msg.from.id}
-`
-    bot.sendMessage(msg.chat.id, markdown, {
-        parse_mode: 'Markdown'
-    })
+ bot.on('message', msg => {
+
+    bot.sendMessage(msg.chat.id, `http://www.aerovis.aero/ru/about-us-ru/`)
 })
 
 
