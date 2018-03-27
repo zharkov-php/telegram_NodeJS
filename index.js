9d26ede8e9392deefa9ee9d70913ad1c1b8924df
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-27 22:06:08 +0300

    При нажатии на ссылку - выводим стикер

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-const TelegramBot = require('node-telegram-bot-api')
+ const TelegramBot = require('node-telegram-bot-api')
  const debug = require('./helpers')
  const TOKEN = '595808254:AAHjcdRI0-wohBLK9_xUf7PV2cbWG_Kse_w'
  const fs = require('fs')
@@ -13,27 +12,17 @@ const TelegramBot = require('node-telegram-bot-api')
     }
 })
 
- bot.onText(/\/doc1/, msg => {
 
-     bot.sendDocument(msg.chat.id, './wfm.xlsx')
 
+ // https://developers.google.com/speed/webp/gallery1
+ bot.onText(/\/s1/, msg => {
+     bot.sendSticker(msg.chat.id, './sticker.webp')
  })
 
-
- bot.onText(/\/doc2/, msg => {
-
-     bot.sendMessage(msg.chat.id, 'Upload start...')
-
-     fs.readFile(__dirname + '/wfm.zip', (err, file) => {
-
-         bot.sendDocument(msg.chat.id, file, {
-             caption: 'Additional text'
-         }).then(() => {
-             bot.sendMessage(msg.chat.id, 'Upload finish')
-         })
-
-     })
-
+ bot.onText(/\/s2/, msg => {
+    fs.readFile(__dirname + '/sticker.webp', (err, sticker) => {
+        bot.sendSticker(msg.chat.id, sticker)
+    })
  })
 
 
