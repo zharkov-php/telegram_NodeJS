a7a97d55e373a4706f1e117ce0567970759725da
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-27 17:27:24 +0300

    Вывод меню-клавиатуры инлайн режим(вывод на экране), вывод кнопки АЛЕРТом и debug

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const TelegramBot = require('node-telegram-bot-api')
 const debug = require('./helpers')
 const TOKEN = '595808254:AAHjcdRI0-wohBLK9_xUf7PV2cbWG_Kse_w'
@@ -13,43 +11,41 @@ const bot = new TelegramBot(TOKEN, {
     }
 })
 
+
 bot.on('message', msg => {
     const chatId = msg.chat.id
 
-    if (msg.text === 'Закрыть'){ //если Кнопка Закрыть
-    bot.sendMessage(chatId, 'Закрываю клавиатуру', {
+    bot.sendMessage(chatId, 'Моя Инлайн клавиатура-меню', {
         reply_markup: {
-            remove_keyboard: true
+            inline_keyboard: [
+               [
+                   {
+                       text: 'Google',//Кнопка гугл ссылка
+                       url: 'https://www.google.com/'
+                   }
+               ],
+                [
+                    {
+                        text: 'Second',// первая кнопка
+                        callback_data: '1'
+                    },
+                    {
+                        text: 'First',//Вторая кнопка
+                        callback_data: 'First'
+                    }
+                ]
+            ]
         }
     })
-    }else if (msg.text === 'Ответить'){ //если Кнопка Ответить
-        bot.sendMessage(chatId, 'Отвечаю Вам', {
-            reply_markup: {
-                force_reply: true
-            }
-        })
-    }else {
-        bot.sendMessage(chatId, 'Моя Клавиатура', {
-            reply_markup: {
-                keyboard: [
-                    [{
-                    text: 'Отправить местоположение',//Кнопка оправить местоположение
-                        request_location: true
-                    }],
-                    ['Ответить', 'Закрыть'],//Кнопки Ответить и Закрыть, работают по условиям выше указанным
-                    [{
-                    text: 'Отправить контакт',//Кнопка Оправить контакт
-                        request_contact: true
-                    }]
-                ],
-                one_time_keyboard: true //ЗАКРЫВАЕМ КЛАВИАТУРУ ПОСЛЕ ВЫБОРА ЛЮБОЙ КНОПКИ
-            }
-        })
-    }
 
 
 
+})
 
+bot.on('callback_query', query => { //вывод результата
+    bot.sendMessage(query.message.chat.id, debug(query))//вывод распечаткой debug
+
+    bot.answerCallbackQuery(query.id, `${query.data}`)//Вывод алертом
 })
 
 
