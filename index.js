3c2d92d330e8f26d1203827c9c5322d9d8ecfb98
Zharkov Andrey <zharkov.php@gmail.com>
2018-03-27 21:50:52 +0300

    При нажатии на ссылку - выводим файл zip и xlsx для загрузки

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
- const TelegramBot = require('node-telegram-bot-api')
+
+const TelegramBot = require('node-telegram-bot-api')
  const debug = require('./helpers')
  const TOKEN = '595808254:AAHjcdRI0-wohBLK9_xUf7PV2cbWG_Kse_w'
  const fs = require('fs')
@@ -12,21 +13,27 @@
     }
 })
 
- // bot.onText(/\/audio/, msg => {
- //
- //   bot.sendAudio(msg.chat.id, './wind_of_change.mp3')
- //
- // })
+ bot.onText(/\/doc1/, msg => {
 
- bot.onText(/\/audio2/, msg => {
+     bot.sendDocument(msg.chat.id, './wfm.xlsx')
 
-     bot.sendMessage(msg.chat.id, 'Start audio uploading...')
+ })
 
-     fs.readFile(__dirname + '/wind_of_change.mp3', (err, data) => {
-         bot.sendAudio(msg.chat.id, data).then(() => {
-             bot.sendMessage(msg.chat.id, 'Uploading finish')
+
+ bot.onText(/\/doc2/, msg => {
+
+     bot.sendMessage(msg.chat.id, 'Upload start...')
+
+     fs.readFile(__dirname + '/wfm.zip', (err, file) => {
+
+         bot.sendDocument(msg.chat.id, file, {
+             caption: 'Additional text'
+         }).then(() => {
+             bot.sendMessage(msg.chat.id, 'Upload finish')
          })
+
      })
+
  })
 
 
